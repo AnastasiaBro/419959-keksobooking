a78d5ec857ce74fb340bdc1fe28c0ec7b4a76b8f
AnastasiaBro <a-sirion@yandex.ru>
2017-11-26 19:29:08 +0300

    Внесла правки

--- js/map.js
+++ js/map.js
@@ -1,12 +1,12 @@
 'use strict';
 
 var NUMBER_OF_ADVERTS = 8;
+var COUNT_FEATURES = 6;
 var similarMapCardTemplate = document.querySelector('template').content;
 var mapCardTemplate = similarMapCardTemplate.querySelector('article.map__card');
 var cityMap = document.querySelector('.map');
 var mapPins = document.querySelector('.map__pins');
 var adverts = [];
-var COUNT_FEATURES = 6;
 cityMap.classList.remove('map--faded');
 
 function getRandomIndex(min, max) {
@@ -19,53 +19,61 @@ function getUniquePart(array) {
 }
 
 function getArrayAdvert(advertNumber) {
-  var NUMBER_AVATAR = ['01', '02', '03', '04', '05', '06', '07', '08'];
-  var TITLE_TEXTS = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-  var HOTEL_TYPES = ['flat', 'house', 'bungalo'];
-  var CHECK_TIMES = ['12:00', '13:00', '14:00'];
-  var COORDINATES_X = [];
-  var COORDINATES_Y = [];
+  var numbers = getPictureNumbers(advertNumber);
+  var titles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+  var types = ['flat', 'house', 'bungalo'];
+  var times = ['12:00', '13:00', '14:00'];
+  var coordinatesX = [];
+  var coordinatesY = [];
+
+  function getPictureNumbers(value) {
+    var numbers = [];
+    for (var i = 1; i <= value; i++) {
+      numbers.push('0' + i);
+    }
+    return numbers;
+  }
 
   (function findCoordinates() {
     for (var i = 0; i < advertNumber; i++) {
-      COORDINATES_X.push(getRandomIndex(300, 900) + 23);
-      COORDINATES_Y.push(getRandomIndex(100, 500) + 62);
+      coordinatesX.push(getRandomIndex(300, 900) + 20);
+      coordinatesY.push(getRandomIndex(100, 500) + 40);
     }
   })();
 
   function findFeatures() {
-    var MANY_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-    var numberRepeat = getRandomIndex(1, MANY_FEATURES.length);
-    var CHOOSE_FEATURES = [];
-    for (var k = 0; k < numberRepeat; k++) {
-      CHOOSE_FEATURES.push(getUniquePart(MANY_FEATURES));
+    var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+    var numberRepeat = getRandomIndex(1, features.length);
+    var choises = [];
+    for (var i = 0; i < numberRepeat; i++) {
+      choises.push(getUniquePart(features));
     }
-    return CHOOSE_FEATURES;
+    return choises;
   }
 
-  for (var j = 0; j < advertNumber; j++) {
+  for (var i = 0; i < advertNumber; i++) {
     adverts.push({
       'author': {
-        'avatar': 'img/avatars/user' + getUniquePart(NUMBER_AVATAR) + '.png',
+        'avatar': 'img/avatars/user' + getUniquePart(numbers) + '.png',
       },
 
       'offer': {
-        'title': '' + getUniquePart(TITLE_TEXTS),
-        'address': '' + COORDINATES_X[j] + ', ' + COORDINATES_Y[j],
+        'title': '' + getUniquePart(titles),
+        'address': '' + coordinatesX[i] + ', ' + coordinatesY[i],
         'price': getRandomIndex(1000, 1000000),
-        'type': HOTEL_TYPES[getRandomIndex(0, HOTEL_TYPES.length - 1)],
+        'type': types[getRandomIndex(0, types.length - 1)],
         'rooms': getRandomIndex(1, 5),
         'guests': getRandomIndex(1, 30),
-        'checkin': '' + CHECK_TIMES[getRandomIndex(0, CHECK_TIMES.length - 1)],
-        'checkout': '' + CHECK_TIMES[getRandomIndex(0, CHECK_TIMES.length - 1)],
+        'checkin': '' + times[getRandomIndex(0, times.length - 1)],
+        'checkout': '' + times[getRandomIndex(0, times.length - 1)],
         'features': findFeatures(),
         'description': '',
         'photos': []
       },
 
       'location': {
-        'x': COORDINATES_X[j],
-        'y': COORDINATES_Y[j]
+        'x': coordinatesX[i],
+        'y': coordinatesY[i]
       }
     });
   }
@@ -84,20 +92,19 @@ function createAllAdverts(advert) {
 function createOneAdvert(advert) {
   var advertElement = mapCardTemplate.cloneNode(true);
 
-  function getTextType() {
-    var text = '';
-    if (advert.offer.type.length !== 4) {
-      text = (advert.offer.type.length === 7) ? 'Бунгало' : 'Дом';
-    } else {
-      text = 'Квартира';
-    }
-    return text;
+  function translatePlaceType(englishType) {
+    var translate = {
+      flat: 'Квартира',
+      house: 'Дом',
+      bungalo: 'Бунгало'
+    };
+    return translate[englishType];
   }
 
   advertElement.querySelector('h3').textContent = advert.offer.title;
   advertElement.querySelector('small').textContent = advert.offer.address;
   advertElement.querySelector('.popup__price').textContent = advert.offer.price + ' ' + String.fromCharCode(8381) + ' / ночь';
-  advertElement.querySelector('h4').textContent = getTextType();
+  advertElement.querySelector('h4').textContent = translatePlaceType(advert.offer.type);
 
   function addCorrectRoomEnding() {
     var ending = '';
@@ -119,13 +126,13 @@ function createOneAdvert(advert) {
 
   (function addFeatures() {
     if (advert.offer.features.length < COUNT_FEATURES) {
-      for (var m = advert.offer.features.length; m < COUNT_FEATURES; m++) {
+      for (var i = advert.offer.features.length; i < COUNT_FEATURES; i++) {
         var extra = advertElement.querySelectorAll('.popup__features li')[advert.offer.features.length];
         advertElement.querySelector('.popup__features').removeChild(extra);
       }
     }
-    for (var n = 0; n < advert.offer.features.length; n++) {
-      advertElement.querySelectorAll('.popup__features li')[n].setAttribute('class', 'feature feature--' + advert.offer.features[n]);
+    for (var i = 0; i < advert.offer.features.length; i++) {
+      advertElement.querySelectorAll('.popup__features li')[i].setAttribute('class', 'feature feature--' + advert.offer.features[i]);
     }
   })();
 
@@ -135,15 +142,17 @@ function createOneAdvert(advert) {
   return advertElement;
 }
 
-(function insertAdvert() {
+function insertAdvert(advertsCount) {
   var fragment = document.createDocumentFragment();
-  getArrayAdvert(NUMBER_OF_ADVERTS);
+  getArrayAdvert(advertsCount);
 
   fragment.appendChild(createOneAdvert(adverts[0]));
   cityMap.appendChild(fragment);
 
-  for (var j = 0; j < NUMBER_OF_ADVERTS; j++) {
-    fragment.appendChild(createAllAdverts(adverts[j]));
+  for (var i = 0; i < advertsCount; i++) {
+    fragment.appendChild(createAllAdverts(adverts[i]));
   }
   mapPins.appendChild(fragment);
-})();
+}
+
+insertAdvert(NUMBER_OF_ADVERTS);

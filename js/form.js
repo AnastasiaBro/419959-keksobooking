307d048ef151aeca4761ee1e1b0abf6bc07ab602
AnastasiaBro <a-sirion@yandex.ru>
2018-01-09 03:54:17 +0300

    Комментарии

--- js/form.js
+++ js/form.js
@@ -113,7 +113,7 @@
     function syncValueWithMin(element, value) {
       element.min = value;
     }
-    window.synchronizeFields(type, price, window.constants.TYPES, PRICES, syncValueWithMin); // types стал TYPES и переехал в constants
+    window.synchronizeFields(type, price, window.constants.TYPES, PRICES, syncValueWithMin); // types стал TYPES и переехал в constants, а prices - PRICES
     checkPriceValidity();
   }
 
